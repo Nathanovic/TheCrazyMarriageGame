383198b171dfad5cb1f9902292642b9d1e0a1caf
Nathan <nathanflier@hotmail.com>
2020-04-25 18:06:32 +0200

    Jump beginning

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,41 @@
 public class Player : MonoBehaviour {
 
 	public float MoveSpeed;
+	public float JumpForce;
+	public Transform GroundCheck;
+	public float GroundCheckDistance;
+	public float MaxJumpButtonCorrectionTime;
 
 	private new Rigidbody2D rigidbody;
+	private bool jumpButtonDown;
+	private float jumpButtonTime;
 
     private void Awake() {
 		rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate() {
+	private void Update() {
+		if (jumpButtonDown) {
+			if ((Time.time - jumpButtonTime) > MaxJumpButtonCorrectionTime) {
+				jumpButtonDown = false;
+			}
+		}
+
+		if (!jumpButtonDown) {
+			jumpButtonDown = Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0);
+			if (jumpButtonDown) {
+				jumpButtonTime = Time.time;
+			}
+		}
+	}
+
+	private void FixedUpdate() {
 		if (!GameManager.Instance.IsPlaying) { return; }
 		rigidbody.AddForce(Vector2.right * MoveSpeed);
+		if (jumpButtonDown) {
+
+			rigidbody.AddForce(Vector2.up * JumpForce);
+		}
     }
 
 }
