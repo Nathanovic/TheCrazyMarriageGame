fce729af198bc32d56f1150426ec2e7ce508fe9d
Nathanovic <n.flier@live.nl>
2020-04-30 13:56:52 +0200

    Added jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,12 +3,18 @@
 public class Player : MonoBehaviour {
 
 	public float MoveSpeed;
+	public float MovementSmoothing;
 	public float JumpForce;
 	public Transform GroundCheck;
+	public LayerMask GroundCheckMask;
+	public SpriteRenderer spriteRenderer;
+	public Vector2 GroundCheckSize;
+	public bool ControlInAir = true;
 	public float GroundCheckDistance;
 	public float MaxJumpButtonCorrectionTime;
 
 	private new Rigidbody2D rigidbody;
+	private bool isGrounded;
 	private bool jumpButtonDown;
 	private float jumpButtonTime;
 
@@ -17,6 +23,7 @@ public class Player : MonoBehaviour {
     }
 
 	private void Update() {
+		if (!GameManager.Instance.IsPlaying) { return; }
 		if (jumpButtonDown) {
 			if ((Time.time - jumpButtonTime) > MaxJumpButtonCorrectionTime) {
 				jumpButtonDown = false;
@@ -33,11 +40,28 @@ public class Player : MonoBehaviour {
 
 	private void FixedUpdate() {
 		if (!GameManager.Instance.IsPlaying) { return; }
-		rigidbody.AddForce(Vector2.right * MoveSpeed);
-		if (jumpButtonDown) {
-
-			rigidbody.AddForce(Vector2.up * JumpForce);
+		bool hitGround = Physics2D.BoxCast(GroundCheck.position, GroundCheckSize, 0f, Vector2.down, GroundCheckDistance, GroundCheckMask);
+		if (hitGround || ControlInAir) {
+			float moveInput = Input.GetAxis("Horizontal");
+			if (moveInput != 0f) {
+				bool lookLeft = moveInput < 0f;
+				spriteRenderer.flipX = lookLeft;
+			}
+			Vector3 targetVelocity = new Vector2(moveInput * MoveSpeed, rigidbody.velocity.y);
+			Vector3 velocity = Vector3.zero;
+			rigidbody.velocity = Vector3.SmoothDamp(rigidbody.velocity, targetVelocity, ref velocity, MovementSmoothing);
+		}
+		
+		if (hitGround && jumpButtonDown) {
+			jumpButtonDown = false;
+			rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0f);
+			rigidbody.AddForce(new Vector2(0f, JumpForce));
 		}
     }
+	
+	private void OnDrawGizmos(){
+		Gizmos.color = Color.gray;
+		Gizmos.DrawLine(GroundCheck.position, GroundCheck.position + Vector3.down * GroundCheckDistance);
+	}
 
 }
